f85cb61de7a2ffe25222523265c0e353fa658b6d
maxim arestov <aresmax1993@gmail.com>
2022-10-30 17:17:19 +0300

    Complete Case1

--- Case1/Program.cs
+++ Case1/Program.cs
@@ -1,2 +1,50 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 1. Написать метод который принимает два числа и меняет значения местами используя ref.
+Console.Clear();
+int a = FillVar();
+int b = FillVar();
+Console.WriteLine($"Введены числа а - {a}, b - {b}");
+SwapValue(ref a, ref b);
+Console.WriteLine($"Значение а после замены - {a}, значение b после замены - {b}");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+int FillVar()
+{
+    int num = 0;
+    Console.WriteLine("Введите число: ");
+    bool check = true;
+    while(check)
+    {
+        bool parse = int.TryParse(Console.ReadLine(), out num);
+        if (!parse)
+        {
+            Console.WriteLine("Введены некорректные данные");
+        }
+        else
+        {
+            check = false; 
+        }
+    }
+    return num;
+}
+void SwapValue(ref int num1, ref int num2)
+{
+    int helper = num1;
+    num1 = num2;
+    num2 = helper;
+}
