cf2448e94913ea06d4b26e3e94dd50b21b14dad1
maxim arestov <aresmax1993@gmail.com>
2022-10-30 17:29:15 +0300

    Complete Case 2

---
+++ Case2/Program.cs
@@ -0,0 +1,42 @@
+﻿//  2. Написать метод который принимает параметр и проверяет четное число или нет
+//  если число четное то вывести результат деления на 2 в параметр с out Возвращающий тип будет bool.
+Console.Clear();
+int a = FillVar();
+if (IsEven(a))
+{
+    Console.WriteLine($"Число четное. При делении на 2 получается {a / 2}");
+}
+else Console.WriteLine("Число нечетное.");
+
+
+
+
+
+
+bool IsEven(int num)
+{
+    if (num % 10 == 0)
+    {
+        return true;
+    }
+    else return false;
+}
+int FillVar()
+{
+    int num = 0;
+    Console.WriteLine("Введите число: ");
+    bool check = true;
+    while (check)
+    {
+        bool parse = int.TryParse(Console.ReadLine(), out num);
+        if (!parse)
+        {
+            Console.WriteLine("Введены некорректные данные");
+        }
+        else
+        {
+            check = false;
+        }
+    }
+    return num;
+}
