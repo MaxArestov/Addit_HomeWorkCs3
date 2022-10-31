0a50a58b48570eec19c27748794b9cfac6887964
maxim arestov <aresmax1993@gmail.com>
2022-10-31 21:47:16 +0300

    Complete Case 4

---
+++ Case4/Program.cs
@@ -0,0 +1,24 @@
+﻿// 4. Сделать метод (аналог TryParse) который принимает на вход строку и параметр с модификатором out и пытается преобразовать в число
+// если получилось то возвращается true, если нет то false
+Console.Clear();
+Console.WriteLine("Введите число:");
+string number = Console.ReadLine();
+int figure = new int();
+Console.WriteLine(StringToInt(number, out figure));
+
+
+
+
+bool StringToInt(string enter, out int export)
+{
+    try
+    {
+        export = Convert.ToInt32(enter);
+        return true;
+    }
+    catch (System.Exception)
+    {
+        export = -0;
+        return false;
+    }
+}
